c3bd68a383ce6740a24a6e024e0ada6d4b6c3099
Jayawickrama Y.R.C.S <chamodansajeewana@gmail.com>
2024-09-23 16:54:31 +0530

    live preview

--- client/src/assets/videoPlayer/VideoPlayer.jsx
+++ client/src/assets/videoPlayer/VideoPlayer.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import ReactPlayer from 'react-player'
 
 const VideoPlayer = ({ url }) => {
-  navigator.mediaDevices
-    .getUserMedia({ video: true })
-    .then((stream) => {
-      const videoElement = document.querySelector('video')
-      videoElement.srcObject = stream
-      videoElement.play()
-    })
-    .catch((error) => console.error('Error accessing webcam: ', error))
+  const webcamRef = useRef(null) // Reference for the webcam video element
+
+  useEffect(() => {
+    const getWebcam = async () => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        })
+        if (webcamRef.current) {
+          webcamRef.current.srcObject = stream // Set the webcam stream to the video element
+          webcamRef.current.play()
+        }
+      } catch (error) {
+        console.error('Error accessing webcam: ', error)
+      }
+    }
+
+    getWebcam()
+  }, []) // Empty dependency array ensures this runs once on component mount
 
   return (
-    <div>
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <video
+        ref={webcamRef}
+        autoPlay
+        width={320}
+        height={240}
+        style={{ marginBottom: '20px' }}
+      />
       <ReactPlayer url={url} controls={true} width={1280} height={720} />
     </div>
   )
